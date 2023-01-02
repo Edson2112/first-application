ae139da035c27be89986cc7d1774a2e28faa23a5
Alex <alexmankiev@gmail.com>
2023-01-02 18:19:48 +0200

    Типы данных

--- src/ua/edson/java/hello/Application.java
+++ src/ua/edson/java/hello/Application.java
@@ -1,14 +1,36 @@
 package ua.edson.java.hello;
 
+import java.util.Scanner;
+
 public class Application {
 
 
     public static void main(String[] args) {
 
-        System.out.println("Hello!!!");
+        // System.out.println("Hello!!!");
 
+//        Scanner in = new Scanner(System.in);
+//
+//        Integer a = in.nextInt();
+//        Integer b = in.nextInt();
+//
+//        System.out.println(a + b);
 
+        Scanner in = new Scanner(System.in);
+
+        Integer a = in.nextInt();
+        //Integer b = vozrast.nextInt();
+
+        if (a < 10) {
+            System.out.println("Ребёнок");
+        } else if (a <= 17) {
+            System.out.println("Парень");
+        } else if (a < 50) {
+            System.out.println("Мужик");
+        } else if (a < 100) {
+            System.out.println("Дед");
+        } else {
+            System.out.println("Cупер Древний");
+        }
     }
-
-
 }
